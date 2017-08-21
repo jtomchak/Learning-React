c7415a96a2209b2d02e48c273d360081823c8c20
Jesse Tomchak <you@example.com>
2017-08-21 16:34:28 -0600

    Removed the class component from Heroes form too!!!
    without a need for the life cycles we can steer clear of the class component all together. sweet!

--- src/components/HeroForm/index.js
+++ src/components/HeroForm/index.js
@@ -1,32 +1,26 @@
 import React, { Component } from "react";
 
-class HeroForm extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
+const HeroForm = props => {
+  return (
+    <div>
       <div>
-        <div>
-          <h2>
-            {this.props.selectedHero.name} details!
-          </h2>
-          <label>id: </label>
-          {this.props.selectedHero.id}
-        </div>
-        <form onSubmit={this.props.handleSubmit()}>
-          <label>name: </label>
-          <input
-            type="text"
-            value={this.props.selectedHero.name}
-            onChange={this.props.handleChange()}
-          />
-          <input className="button" type="submit" value="Submit" />
-        </form>
+        <h2>
+          {props.selectedHero.name} details!
+        </h2>
+        <label>id: </label>
+        {props.selectedHero.id}
       </div>
-    );
-  }
-}
+      <form onSubmit={props.handleSubmit()}>
+        <label>name: </label>
+        <input
+          type="text"
+          value={props.selectedHero.name}
+          onChange={props.handleChange()}
+        />
+        <input className="button" type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
 
 export default HeroForm;
