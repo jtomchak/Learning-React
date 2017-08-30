072e3713a22b065b4c587230c5b088d59ea1951e
Jesse Tomchak <you@example.com>
2017-08-30 05:11:06 -0700

    refactored our app into heroes, so that we can do our routing from the app shell

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,98 +1,17 @@
 import React, { Component } from "react";
+import Heroes from "../Heroes";
 
-import HeroForm from "../HeroForm";
-import HeroList from "../HeroList";
-import { getHeroes, getHeroesSlowly } from "../../services/hero.service";
 import "./App.css";
-
-const DEFAULT_NO_HERO = {
-  name: "",
-  id: undefined
-};
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      title: "Tour of Git Heros",
-      heroes: [],
-      selectedHero: DEFAULT_NO_HERO
-    };
-
-    //Binding is no longer inheriantly done with extends component
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.selectHero = this.selectHero.bind(this);
-  }
-
-  /*
-  We need to now 'call' for our heros when this compoent is going to be mounted
-  */
-  componentWillMount() {
-    getHeroesSlowly.then(payload => {
-      this.setState({
-        heroes: payload
-      });
-    });
-  }
-  /*
-  capture the index of the selected hero for handleChange
-  also if the id of hero param is the current selectedHero, reset it
-  */
-  selectHero(hero) {
-    const heroIndex = this.state.heroes.map(o => o.id).indexOf(hero.id);
-    hero = this.state.selectedHero.id !== hero.id ? hero : DEFAULT_NO_HERO;
-    this.setState({
-      selectedHero: {
-        ...hero,
-        index: heroIndex
-      }
-    });
-  }
-
-  //Object spread operator over hero object from state
-  handleChange(event) {
-    this.setState({
-      selectedHero: {
-        ...this.state.selectedHero,
-        name: event.target.value
-      }
-    });
-  }
-
-  handleSubmit(event) {
-    //Sweet mother of mary!! what is going on here ?!?!
-    //So much to keep it from mutating
-    this.setState({
-      heroes: [
-        ...this.state.heroes.slice(0, this.state.selectedHero.index),
-        { id: this.state.selectedHero.id, name: this.state.selectedHero.name },
-        ...this.state.heroes.slice(
-          this.state.selectedHero.index + 1,
-          this.state.heroes.length
-        )
-      ],
-      selectedHero: DEFAULT_NO_HERO
-    });
-    event.preventDefault();
   }
 
   render() {
     return (
       <div>
-        <h1>{this.state.title}</h1>
-        <HeroList
-          heroes={this.state.heroes}
-          selectedHero={this.state.selectedHero}
-          onHeroClick={this.selectHero}
-        />
-        {this.state.selectedHero.name && (
-          <HeroForm
-            selectedHero={this.state.selectedHero}
-            handleChange={() => this.handleChange}
-            handleSubmit={() => this.handleSubmit}
-          />
-        )}
+        <Heroes />
       </div>
     );
   }
