9fcdeb94a3d57e6c4ddf3d7a5c8896c047829fad
Jesse Tomchak <you@example.com>
2017-08-30 06:05:14 -0700

    now we've got a dashboard with dynamic url for hero details

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,5 @@
 import React, { Component } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  NavLink
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 import Heroes from "../Heroes";
 import Dashboard from "../Dashboard";
 
--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,5 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./Dashboard.css";
 
-const Dashboard = () => <div>Oh Hi!</div>;
+import HeroDetails from "../Heroes/HeroForm";
+import { getHeroes, getHeroesSlowly } from "../../services/hero.service";
+
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      heroes: []
+    };
+  }
+
+  componentWillMount() {
+    getHeroes.then(payload => {
+      this.setState({
+        heroes: payload
+      });
+    });
+  }
+
+  render() {
+    const heroBlocks = this.state.heroes.map(hero => (
+      <Link key={hero.id} className="col-1-4" to={`/details/${hero.id}`}>
+        <div className="module hero">
+          <h4>{hero.name}</h4>
+        </div>
+      </Link>
+    ));
+    return (
+      <div>
+        <h3>Top Heroes</h3>
+        <div className="grid grid-pad">{heroBlocks}</div>
+      </div>
+    );
+  }
+}
 
 export default Dashboard;
